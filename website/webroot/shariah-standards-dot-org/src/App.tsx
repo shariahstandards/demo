a3a90134299f49e4c91e1c2e44f27f47cb298cd1
Lamaan Ball <lamaan@lamaan.com>
2022-12-23 08:04:58 +0000

    react app under construction place holder

---
+++ website/webroot/shariah-standards-dot-org/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/logo.svg" width={100} alt="shariah standards"/>
+      </header>
+      <div style={{fontSize:"20pt"}}>
+        Shariah Standards 
+      </div>
+      <div>
+        Under Construction
+      </div>
+    </div>
+  );
+}
+
+export default App;
